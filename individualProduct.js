50a356468530b7171af75903287c924e929e4868
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2021-12-17 12:30:55 +0530

    made cart button resposnive and connected it to cart page. Shows cart in a small pop div in the indiviudal product page when product added to cart

--- individualProduct.js
+++ individualProduct.js
@@ -11,11 +11,12 @@ var img = document.createElement("img");
 img.setAttribute("src",product.imgUrl);
 
 var strikedprc = document.createElement("p");
-strikedprc.textContent = product.strikedOffPrice;
+strikedprc.textContent = "₹" +product.strikedOffPrice;
 strikedprc.style.textDecoration = "line-through";
+strikedprc.style.color = "grey"
 
 var prc = document.createElement("p");
-prc.textContent = product.price;
+prc.textContent = "₹"+ product.price;
 
 var discount = document.createElement("p");
     var x = Math.floor(((product.strikedOffPrice-product.price)/product.strikedOffPrice)*100)
@@ -38,11 +39,51 @@ document.querySelector("#addToCart").addEventListener("click",function(){
         product_name: product.name,
         product_price: product.price,
         quantity: qty,
-    } 
-    cart.push(cartobj);
+    }
+    var obj_found = false
+    for(var i =0; i<cart.length; i++){
+        if(cartobj.product_name == cart[i].product_name){
+            cart[i].quantity++;
+            obj_found = true;
+            break;
+        }
+    }
+    if(obj_found==false){
+        cart.push(cartobj);
+    }
+    
     localStorage.setItem("shoppingCart",JSON.stringify(cart));
+    document.querySelector("#counter").textContent = cart.length;
+    
+    if(window.matchMedia("(min-width:800px)").matches){
+        var cartDisplay = document.querySelector("#cDisplay");
+        cartDisplay.innerHTML = "";
+        cart.map(function(elem){        
+            var div=document.createElement("div");
+            div.setAttribute("id","cartshowdiv");       
+
+            var img = document.createElement("img");
+            img.setAttribute("src",elem.image);
+            img.setAttribute("width","150px");
+            var nm = document.createElement("p");
+            nm.textContent = elem.product_name;
+            var p = document.createElement("p");
+            p.textContent = "₹"+elem.product_price;
+            var q = document.createElement("p");
+            q.textContent = "QTY:"+elem.quantity;
+            div.append(img, nm, p, q);
+            
+            cartDisplay.append(div);
+            document.querySelector("#cartDisplay").style.display = "block";
+        })
+
+    }
+    
 })
 
+document.querySelector("#closeCart").addEventListener("click",function(){
+    document.querySelector("#cartDisplay").style.display = "none";
+})
 
 
 document.querySelector("#qtyplus").addEventListener("click",function(){
@@ -56,3 +97,5 @@ document.querySelector("#qtyminus").addEventListener("click",function(){
     
     document.querySelector("#qty").textContent = count;
 })  
+
+document.querySelector("#counter").textContent = cart.length;
